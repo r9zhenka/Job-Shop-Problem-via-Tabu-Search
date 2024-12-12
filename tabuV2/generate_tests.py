889fd6d7da67cd4853b2563acada4d8b87018fd7
boooobs <113143128+r9zhenka@users.noreply.github.com>
2024-12-12 23:34:43 +0300

    Update generate_tests.py

--- tabuV2/generate_tests.py
+++ tabuV2/generate_tests.py
@@ -1,53 +1,45 @@
 import os
 import random
 
-
-TEST_COUNT = 1
-MACHINES = 60
-MAX_TIME = 100
+TEST_COUNT = 20
+MACHINES = 10
+MAX_TIME = 10
 JOBS = 10
 
 
-def GenerateJob() -> str:
-    job = "["
+def GenerateJob(NUM_MACHINES) -> str:
+    job = []
 
-    for machine in range(MACHINES):
-        if random.randint(0, 1) == 0: continue
-
-        job += f"[{machine}, {random.randint(1, MAX_TIME)}]"
-
-        if machine != MACHINES - 1:
-            job += ','
-
-    job += "]"
+    for machine in range(NUM_MACHINES):
+        if machine!=1 and random.randint(0, 1) == 0: continue
+        job.append([machine, random.randint(1, MAX_TIME)])
     return job
 
 
-def GenerateTest() -> str:
-    test = "{\"jobs_data\": ["
+def GenerateTest(i) -> str:
+    test = []
+    for _ in range(JOBS):
+        test.append(GenerateJob(i))
 
-    for i in range(JOBS):
-        test += GenerateJob()
-        if i != JOBS - 1:
-            test += ","
-
-    test += "]}"
-    return test
+    return '{\"jobs_data\": ' + str(test) + '}'
 
 
 if __name__ == "__main__":
-    existing_files = os.listdir("tests/")
+    existing_files = os.listdir("tests_for_graphics/")
 
     count = 0
     current = 0
+    JOBS = 20
     while count < TEST_COUNT:
-        filename = f"{current}.json"
-        current += 1
+        for i in range(3, MACHINES+1):
+            filename = f"{current}.json"
+            current += 1
+            count += 1
 
-        if filename in existing_files: continue
+            if filename in existing_files: continue
 
-        file = open("tests/" + filename, 'w')
-        file.write(GenerateTest())
-        file.close()
+            file = open("tests_for_graphics/" + filename, 'w')
+            file.write(GenerateTest(i))
+            file.close()
 
-        count += 1
+
