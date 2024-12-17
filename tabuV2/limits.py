4efbee06ac617d8e45ffd3b085ee2e1ed2d635b3
oughhhhh <oughhhhh@proton.me>
2024-12-17 13:44:43 +0300

    limits

--- tabuV2/limits.py
+++ tabuV2/limits.py
@@ -8,12 +8,18 @@ from google_solver import *
 
 iterations = 20
 tabuListSize = 5
+count = 0
+
+medians = [[] for _ in range(iterations)]
 
 testsPath = "tests/"
 if __name__ == "__main__":
     for filename in os.listdir(testsPath):
         if not filename.endswith(".json"): continue
         try:
+            count += 1
+            # if count > 5: break
+
             file = open(testsPath + filename, 'r')
             jobsData = json.loads(file.read())["jobs_data"]
             file.close()
@@ -26,8 +32,16 @@ if __name__ == "__main__":
             plt.plot(plotX, [(hist - benchmark) / benchmark for hist in tabu.history])
             plt.plot(plotX, [0 for _ in range(len(tabu.history))])
 
+            for i in range(len(tabu.history)):
+                medians[i].append( (tabu.history[i] - benchmark) / benchmark )
+
         except Exception as ex:
             print(filename, "Skipped due to", ex)
             continue
+
+    for median in medians:
+        median.sort()
+
+    plt.plot([i for i in range(iterations)], [median[len(median) // 2] for median in medians], linestyle="dotted")
 
     plt.show()
