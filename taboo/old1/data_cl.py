726a8edf9c867c3710b996c356425f1cc6d0e6ee
oughhhhh <oughhhhh@proton.me>
2024-12-27 20:03:30 +0300

    cleanup

--- taboo/old1/data_cl.py
+++
@@ -1,9 +0,0 @@
-import json
-import random
-data = {
-    f'task{i+1}': [random.randint(1, 10) for _ in range(random.randint(1, 3))]
-    for i in range(100)
-}
-with open('data.json', 'w') as f:
-    json.dump(data, f)
-    f.close()
